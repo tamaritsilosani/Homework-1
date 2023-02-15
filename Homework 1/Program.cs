23f90afb255fa114ddec7a21ba23a4919ae97035
tamari.tsilosani <tamari.tsilosani@TTSILOSANI>
2023-02-15 17:34:20 +0400

    Add project files.

---
+++ Homework 1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*ვიწყებ პროგრამირებას, ჩემი მიზანია:
+ * 6 თვეში გავხდე ჯუნიორ დეველოპერი
+ * გამოვიდე კარგი გოგო
+ * მქონდეს მაღალი ანაზღაურება
+ * ვიმოგზაურო მთელს მსოფლიოში
+ */
+
+
+class TamunaTsilosani
+{
+static void Main(String[] args)
+    {
+        //tststststst
+        Console.WriteLine("Hello world");
+    }
+}
